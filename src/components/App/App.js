49f68065c4ce4a9cd20c8fd8cdb6d9f0faf95cb3
GannaKov <104867435+GannaKov@users.noreply.github.com>
2023-07-18 19:13:15 +0200

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import { AppWrapper } from "./App.styled";
+import { GlobalStyle } from "globalStyles";
+function App() {
+  return (
+    <AppWrapper>
+      <p>My React Template</p>
+      <GlobalStyle />
+    </AppWrapper>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+  margin: 0 auto;
+  height: 100vh;
+  padding: 20px;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 45px;
+  color: #010101;
+`;
